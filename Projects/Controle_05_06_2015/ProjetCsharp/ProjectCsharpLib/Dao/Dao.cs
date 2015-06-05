e1cb9a8e764d6746dac3a8fdfff60f0c9339ad9a
alandrieu <alandrieu@t2u.com>
2015-06-05 15:17:41 +0200

    Hibernate Start

---
+++ Projects/Controle_05_06_2015/ProjetCsharp/ProjectCsharpLib/Dao/Dao.cs
@@ -0,0 +1,61 @@
+﻿using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectCsharpLib.Dao
+{
+    /// <summary>
+    /// Accès à la base de données via Hibernate
+    /// </summary>
+    public class Dao
+    {
+        public NHibernate.ISessionFactory sessionsController;
+        public NHibernate.ISession session;
+
+        public static Dao CurrentDao { get; set; }
+
+        Configuration cfg;
+
+        public Dao()
+        {
+            // Init Cave
+            // Initialize NHibernate
+            cfg = new Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(typeof(Bean.Contact).Assembly);
+            //new SchemaExport(cfg).Execute(true, true, false);
+
+            // Get ourselves an NHibernate Session
+            //var sessions = cfg.BuildSessionFactory();
+            sessionsController = cfg.BuildSessionFactory();
+
+            session = sessionsController.OpenSession();
+
+            for (int i = 0; i < 10; i++)
+                createContact();
+        }
+
+        /// <summary>
+        /// Méthode qui créer un utilisateur par défaut pour la démonstration
+        /// </summary>
+        private void createContact()
+        {
+            Random rand = new Random();
+
+            // Créer un Vendeur
+            Bean.Contact contact = new Bean.Contact();
+            //{
+            //    Name = "Isitech Account",
+            //    Login = "ISITECH",
+            //    Password = Hashing.SHA512("ISITECH")
+            //};
+
+            // And save it to the database
+            session.Save(contact);
+            session.Flush();
+        }
+    }
+}
---
+++ Projects/Controle_05_06_2015/ProjetCsharp/ProjectCsharpLib/Service/ContactService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ProjectCsharpLib.Dao;
+using ProjectCsharpLib.Bean;
+using NHibernate.Criterion;
+
+namespace ProjectCsharpLib.Service
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class ContactService
+    {
+        private ProjectCsharpLib.Dao.Dao currentDao;
+
+        public ContactService()
+        {
+            this.currentDao = ProjectCsharpLib.Dao.Dao.CurrentDao;
+        }
+
+        /// <summary>
+        /// Retrouver un et un seul Vendeur (via son Login)
+        /// </summary>
+        /// <param name="oVendeur"></param>
+        /// <returns></returns>
+        public IList<Bean.Contact> FindVendeur(Contact oContact)
+        {
+            // Note that we do not use the table name specified
+            // in the mapping, but the class name, which is a nice
+            // abstraction that comes with NHibernate
+            //IQuery q = currentDao.session.CreateQuery("FROM Vendeur");
+            //var list = q.List<Bean.Vendeur>();
+
+            //// List all the entries' names
+            //list.ToList().ForEach(p => Console.WriteLine(p.Name));
+
+            ////
+            //Console.Out.WriteLine("Next : " + Environment.NewLine);
+
+            //var users = currentDao.sessionsController.Query<Vendeur>()
+            //       .Where(x => x.UserName == "Abcd" && u.Password == "123456");
+            //return null;
+
+            IList<Bean.Contact> users = currentDao.session.CreateCriteria<Contact>()
+                   .Add(Restrictions.Eq("Nom", oContact.Nom))
+                   .List<Contact>();
+
+            return users;
+        }
+    }
+}
